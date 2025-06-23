fdc4d8ae2ee26c9c395860655eccb58629dea0e2
Pavel Roslovets <p.v.roslovets@gmail.com>
2025-06-23 22:10:24 +0200

    Fix Russian jpg example

--- js/i18n.js
+++ js/i18n.js
@@ -51,36 +51,62 @@ var levelTranslations = {
     {
       helpTitle: 'Выбор элементов по типу',
       doThis: 'Выберите тарелки',
-      help: 'Выбирает все элементы типа <strong>A</strong>. Тип соответствует тегу: <tag>div</tag>, <tag>p</tag> и <tag>ul</tag> — разные типы.'
+      help: 'Выбирает все элементы типа <strong>A</strong>. Тип соответствует тегу: <tag>div</tag>, <tag>p</tag> и <tag>ul</tag> — разные типы.',
+      examples: [
+        '<strong>//div</strong> выбирает все элементы <tag>div</tag>.',
+        '<strong>//p</strong> выбирает все элементы <tag>p</tag>.'
+      ]
     },
     {
       doThis: 'Выберите коробочки бенто',
-      help: 'Выбирает все элементы типа <strong>A</strong>. Тип соответствует тегу: <tag>div</tag>, <tag>p</tag> и <tag>ul</tag> — разные типы.'
+      help: 'Выбирает все элементы типа <strong>A</strong>. Тип соответствует тегу: <tag>div</tag>, <tag>p</tag> и <tag>ul</tag> — разные типы.',
+      examples: [
+        '<strong>//div</strong> выберет все элементы <tag>div</tag>.',
+        '<strong>//p</strong> выберет все элементы <tag>p</tag>.'
+      ]
     },
     {
       doThis: 'Выберите красивую тарелку',
       helpTitle: 'Выбор элементов с атрибутом',
-      help: 'Выбирает элемент по атрибуту <strong>id</strong>. Селектор ID можно сочетать с выбором по типу. Работает и для других атрибутов, например <strong>class</strong>, <strong>name</strong>, <strong>placeholder</strong>.'
+      help: 'Выбирает элемент по атрибуту <strong>id</strong>. Селектор ID можно сочетать с выбором по типу. Работает и для других атрибутов, например <strong>class</strong>, <strong>name</strong>, <strong>placeholder</strong>.',
+      examples: [
+        '<strong>//*[@id="Element ID"]</strong> выберет любой элемент с <strong>id="cool"</strong>',
+        '<strong>//ul[@id="long"]</strong> выберет <strong>&lt;ul id="long"&gt;</strong>'
+      ]
     },
     {
       helpTitle: 'Выбор элемента внутри другого',
       doThis: 'Выберите яблоко на тарелке',
-      help: 'Выбирает все <strong>B</strong> внутри <strong>A</strong>. Здесь <strong>B</strong> — потомок, то есть элемент, находящийся внутри другого.'
+      help: 'Выбирает все <strong>B</strong> внутри <strong>A</strong>. Здесь <strong>B</strong> — потомок, то есть элемент, находящийся внутри другого.',
+      examples: [
+        '<strong>//p/strong</strong> выберет все <strong>&lt;strong&gt;</strong>, которые являются потомками любого <strong>&lt;p&gt;</strong>',
+        '<strong>//*[@id="fancy"]/span</strong> выберет любой <strong>&lt;span&gt;</strong>, являющийся потомком элемента с <strong>id="fancy"</strong>'
+      ]
     },
     {
       doThis: 'Выберите огурец на красивой тарелке',
       helpTitle: 'Комбинация Descendant и ID селекторов',
-      help: 'Любой селектор можно сочетать с селектором потомка.'
+      help: 'Любой селектор можно сочетать с селектором потомка.',
+      examples: [
+        '<strong>//*[@id="cool"]/span</strong> выберет все элементы <strong>&lt;span&gt;</strong> внутри элементов с <strong>id="cool"</strong>'
+      ]
     },
     {
       doThis: 'Выберите маленькие яблоки',
       helpTitle: 'Выбор по классу',
-      help: 'Функция contains выбирает элементы с атрибутом, содержащим указанное значение. У элемента может быть только один ID, но классов может быть несколько.'
+      help: 'Функция contains выбирает элементы с атрибутом, содержащим указанное значение. У элемента может быть только один ID, но классов может быть несколько.',
+      examples: [
+        '<strong>//*[contains(@class,"neato")]</strong> выбирает все элементы с <strong>class="neato"</strong>'
+      ]
     },
     {
       doThis: 'Выберите маленькие апельсины',
       helpTitle: 'Более точный выбор по атрибуту',
-      help: 'Селекторы позволяют делать более точный выбор.'
+      help: 'Селекторы позволяют делать более точный выбор.',
+      examples: [
+        '<strong>//ul[contains(@class,"important")]</strong> выберет все элементы <strong>&lt;ul&gt;</strong> с <strong>class="important stuff"</strong>',
+        '<strong>//input[@placeholder="Name"]</strong> выберет все элементы <strong>&lt;input&gt;</strong> с <strong>placeholder="Name"</strong>'
+      ]
     },
     {
       doThis: 'Выберите маленькие апельсины в бенто',
@@ -91,55 +117,95 @@ var levelTranslations = {
     {
       doThis: 'Выберите все тарелки и бенто',
       helpTitle: 'Комбинирование селекторов',
-      help: '<strong>//A</strong> | <strong>//B</strong> — так можно объединять любые селекторы и перечислять больше двух.'
+      help: '<strong>//A</strong> | <strong>//B</strong> — так можно объединять любые селекторы и перечислять больше двух.',
+      examples: [
+        '<strong>//p| //*[@id="fun"]</strong> выберет все элементы <tag>p</tag> и все элементы с <strong>id="fun"</strong>',
+        '<strong>//a|//p|//div</strong> выберет все элементы <tag>a</tag>, <tag>p</tag> и <tag>div</tag>'
+      ]
     },
     {
       doThis: 'Выберите всё на тарелке',
       helpTitle: 'Универсальный селектор',
-      help: 'Выберет все элементы внутри <strong>A</strong>.'
+      help: 'Выберет все элементы внутри <strong>A</strong>.',
+      examples: [
+        '<strong>//p/*</strong> выберет каждый элемент внутри всех элементов <strong>&lt;p&gt;</strong>',
+        '<strong>//ul[@id="fancy"]/*</strong> выберет каждый элемент внутри всех элементов <strong>&lt;ul id="fancy"&gt;</strong>'
+      ]
     },
     {
       doThis: 'Выберите каждое яблоко рядом с тарелкой',
       helpTitle: 'Соседний селектор',
-      help: 'Выбирает все элементы <strong>B</strong>, которые непосредственно следуют за <strong>A</strong>. Такие элементы называют соседними и они находятся на одном уровне.'
+      help: 'Выбирает все элементы <strong>B</strong>, которые непосредственно следуют за <strong>A</strong>. Такие элементы называют соседними и они находятся на одном уровне.',
+      examples: [
+        '<strong>//p/following-sibling::div</strong> выберет каждый элемент <strong>&lt;div&gt;</strong>, который непосредственно следует за <tag>p</tag>',
+        '<strong>//div//following-sibling::a</strong> выберет каждый элемент <tag>a</tag>, который непосредственно следует за <tag>div</tag>'
+      ]
     },
     {
       doThis: 'Выберите третий огурец',
       helpTitle: 'Селектор по индексу',
-      help: 'Можно выбрать конкретный элемент из результата XPath.'
+      help: 'Можно выбрать конкретный элемент из результата XPath.',
+      examples: [
+        '<strong>(//a)[2]</strong> выберет второй элемент <strong>A</strong>'
+      ]
     },
     {
       doThis: 'Выберите яблоко прямо на тарелке',
       helpTitle: 'Селектор прямых детей',
-      help: 'Можно выбрать элементы, которые являются прямыми потомками других элементов. Более глубокие элементы называются descendant.'
+      help: 'Можно выбрать элементы, которые являются прямыми потомками других элементов. Более глубокие элементы называются descendant.',
+      examples: [
+        '<strong>A//B</strong> выберет все <strong>B</strong>, которые являются прямыми детьми <strong>A</strong>'
+      ]
     },
     {
       doThis: 'Выберите большое яблоко и большой апельсин',
       helpTitle: 'Выбор последнего элемента',
-      help: 'Этот селектор позволяет выбрать последний дочерний элемент.\u00a0Совет: если элемент один, он считается и первым, и единственным, и последним.'
+      help: 'Этот селектор позволяет выбрать последний дочерний элемент.\u00a0Совет: если элемент один, он считается и первым, и единственным, и последним.',
+      examples: [
+        '<strong>(//div)[last()]</strong> выбирает последний элемент <strong>&lt;div&gt;</strong>',
+        '<strong>//div/*[last()]</strong> выбирает все последние элементы внутри <strong>&lt;div&gt;</strong>'
+      ]
     },
     {
       doThis: 'Выберите элементы с атрибутом',
       helpTitle: 'Выбор элементов с атрибутом',
-      help: 'Атрибуты указываются в открывающем теге элемента, например <tag>span attribute="value"</tag>. Атрибут может быть и без значения.'
+      help: 'Атрибуты указываются в открывающем теге элемента, например <tag>span attribute="value"</tag>. Атрибут может быть и без значения.',
+      examples: [
+        '<strong>//a[@href]</strong> выбирает все элементы <tag>a</tag> с атрибутом <strong>href="anything"</strong>',
+        '<strong>//*[@type]</strong> выбирает все элементы с атрибутом <strong>type="anything"</strong>'
+      ]
     },
     {
       doThis: 'Выберите тарелки для кого-нибудь',
       helpTitle: 'Выбор элементов с атрибутом',
-      help: 'Селектор по атрибуту можно объединять с другими, например с именем тега, добавив его в конец.'
+      help: 'Селектор по атрибуту можно объединять с другими, например с именем тега, добавив его в конец.',
+      examples: [
+        '<strong>//*[@value]</strong> выбирает все элементы с атрибутом <strong>value="anything"</strong>',
+        '<strong>//a[@href]</strong> выбирает все элементы <tag>a</tag> с атрибутом <strong>href="anything"</strong>',
+        '<strong>//input[@disabled]</strong> выбирает все элементы <tag>input</tag> с атрибутом <strong>disabled</strong>'
+      ]
     },
     {
       doThis: 'Выберите блюдо Виталия',
       helpTitle: 'Выбор по значению атрибута',
-      help: 'Значения атрибутов учитывают регистр, совпадать должны все символы.'
+      help: 'Значения атрибутов учитывают регистр, совпадать должны все символы.',
+      examples: [
+        '<strong>//input[@type="checkbox"]</strong> выбирает все элементы ввода типа checkbox'
+      ]
     },
     {
       doThis: "Выберите блюда на имена, начинающиеся с 'Sa'",
-      helpTitle: 'Значение атрибута начинается с'
+      helpTitle: 'Значение атрибута начинается с',
+      examples: [
+        '<strong>//toy[starts-with(@category,"Swim")]</strong> выбирает элементы <tag>toy</tag> с <strong>category="Swimwear"</strong> или <strong>category="Swimming"</strong>'
+      ]
     },
     {
       doThis: "Выберите блюда на имена, оканчивающиеся на 'ato'",
-      helpTitle: 'Значение атрибута оканчивается на'
+      helpTitle: 'Значение атрибута оканчивается на',
+      examples: [
+        '<strong>//img[substring(@src, string-length(@src) - string-length(\'.jpg\')+1 ) = \'jpg\']</strong> выбирает все изображения с форматом <strong>.jpg</strong>'
+      ]
     }
   ]
 };
